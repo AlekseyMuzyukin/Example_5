8805f771fdb9a4edf78c1dab4cc042a5c6967c1d
Aleksey <J2k_pot@mail.ru>
2022-09-30 18:50:58 +0300

    add 36

---
+++ Task_36/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите размер массива ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] Array1 = new int[a];
+int sum = 0;
+
+InputArray(Array1);
+PrintArray(Array1);
+SumOddPosition(Array1);
+
+void InputArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i <= array.Length - 1; i++) array[i] = rnd.Next(-100, 100);
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i <= array.Length - 2; i++) Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+
+void SumOddPosition(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        sum = sum + array[i];
+    }
+   
+}
+
+
+Console.WriteLine ($" -> сумма нечётных элементов {sum}");
